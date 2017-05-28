111f72174034103bb1987ea5e5ebe193736d0d3e
zjhuang <zjhuang@ZJteki-MacBook-Pro.local>
2017-05-28 14:21:02 +0800

    Fixed ocument.getElementById  error

--- public/sign_up.js
+++ public/sign_up.js
@@ -10,15 +10,21 @@ messagingSenderId: "668265784196"
 firebase.initializeApp(config);
 
 var account = document.getElementById("account");
+//取得account input
 var pwd = document.getElementById("pwd");
+//取得pwd input
 var registerSmtBtn = document.getElementById("registerSmtBtn");
+//取得 register button
 registerSmtBtn.addEventListener("click", function(){
-        console.log(account.value);
+//將regiser button 加上 click event    
+        console.log("Register a new account:", account.value);
     firebase.auth().createUserWithEmailAndPassword(account.value, pwd.value).catch(function(error) {
+    
     // Handle Errors here.
 
     var errorCode = error.code;
     var errorMsg = error.message;
     console.log(errorMsg);
+    document.getElementById("register_error").innerText = errorMsg;
 });
 },false); 
